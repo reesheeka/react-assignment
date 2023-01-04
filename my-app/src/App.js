1b85b24ca640944c3e244eeecced18a271f69973
reesheeka <rishikasingh9525@gmail.com>
2023-01-04 23:16:04 +0530

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,66 @@
-import React from 'react';
-import './index.css';
+import React, { useState } from "react";
+import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      showParagraph: false,
-    };
+function App() {
+  const [firstName,setFirstName]=useState('')
+  const [lastName,setLastName]=useState('')
+  const [gender,setGender]=useState('')
+  const [country,setCountry]=useState('')
+  const [marketing_emails,setmarketing_emails]=useState('')
+  const [profile_picture,setprofile_picture]=useState('')
+  const [error,setError]=useState(false)
+
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(firstName.length==0||lastName.length==0 || gender.length==0 ||country.length==0 || marketing_emails.length==0 || profile_picture.length==0 ){
+          setError(true)
+      }
+      if(firstName&&lastName&&gender&&country&&marketing_emails&&profile_picture)
+      {
+      console.log("First Name: ",firstName,"\nLast Name: ",lastName, "\nGender:",gender,"\nCountry:",country,"\nMarketing Emails:",marketing_emails,"\nProfile Picture:",profile_picture)
+      }
   }
-  render() {
-    return (
-      <>
-        <textarea
-          value={this.state.input}
-          onChange={(e) => this.setState({ input: e.target.value })}
-        />
-        <br />
-        <button
-          onClick={() => {
-            this.setState({
-              showParagraph: !this.state.showParagraph,
-            });
-          }}
-        >
-          Display in uppercase
-        </button>
-        {this.state.showParagraph && <p>{this.state.input.toUpperCase()}</p>}
-        
-      </>
-    );
-  }
+  return (
+    <>
+           <form onSubmit={handleSubmit}>
+               <div>
+                   <input placeholder="First Name" onChange={e=>setFirstName(e.target.value)} />
+               </div>
+               {error&&firstName.length<=0?
+               <label>First Name can't be Empty</label>:""}
+               <div>
+                   <input placeholder="Last Name" onChange={e=>setLastName(e.target.value)} />
+               </div>
+               {error&&lastName.length<=0?
+               <label>Last Name can't be Empty</label>:""}
+               <div>
+                   <input placeholder="Gender" onChange={e=>setGender(e.target.value)} />
+               </div>
+               {error&&gender.length<=0?
+               <label>Gender can't be Empty</label>:""}
+               <div>
+                   <input placeholder="Country" onChange={e=>setCountry(e.target.value)} />
+               </div>
+               {error&&country.length<=0?
+               <label>Country can't be Empty</label>:""}
+               <div>
+                   <input placeholder="Marketing Emails" onChange={e=>setmarketing_emails(e.target.value)} />
+               </div>
+               {error&&marketing_emails.length<=0?
+               <label>Marketing Emails can't be Empty</label>:""}
+               <div>
+                   <input placeholder="Profile Picture" onChange={e=>setprofile_picture(e.target.value)} />
+               </div>
+               {error&&profile_picture.length<=0?
+               <label>Profile Picture can't be Empty</label>:""}
+               <div>
+                   <button>
+                       Submit
+                   </button>
+               </div>
+           </form>
+        </>
+  );
 }
 
 export default App;
